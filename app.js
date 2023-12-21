a0a50f82899ef02fbe5d5f26cd61ad8fb140cb24
ivancotacte <cotactearmenion@gmail.com>
2023-12-21 19:58:25 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
